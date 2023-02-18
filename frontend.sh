9df8a0c1163ead83055a55f8273fefb4db4a1427
Raghu K <raghuk.vit@gmail.com>
2023-02-18 08:01:52 +0530

    Update roboshop-shell/

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,28 @@
 code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
 
 echo -e "\e[35mInstalling nginx\e[0m"
-yum install nginx -y
+yum install nginx -y &>>${log_file}
 
 echo -e "\e[35mRemoving Old Content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 
 echo -e "\e[35mDownloading Frontend Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
 echo -e "\e[35mExtracting Downloaded Frontend\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log_file}
 
 echo -e "\e[35mCopying Nginx Config for RoboShop\e[0m"
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 echo -e "\e[35mEnabling nginx\e[0m"
-systemctl enable nginx
+systemctl enable nginx &>>${log_file}
 
 echo -e "\e[35mStarting nginx\e[0m"
-systemctl restart nginx
+systemctl restart nginx &>>${log_file}
 
-## RoboShop Config is not copied
+
 ## If any command is errored or failed, we need to stop the script

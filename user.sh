906243817777be7d7066a8cbfc972fa4b659714b
Raghu K <raghuk.vit@gmail.com>
2023-02-20 08:43:23 +0530

    Update roboshop-shell/

---
+++ user.sh
@@ -0,0 +1,67 @@
+source common.sh
+
+print_head "Configure NodeJS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_head "Install NodeJS"
+yum install nodejs -y &>>${log_file}
+status_check $?
+
+print_head "Create Roboshop User"
+id roboshop  &>>${log_file}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${log_file}
+fi
+status_check $?
+
+print_head "Create Application Directory"
+if [ ! -d /app ]; then
+  mkdir /app &>>${log_file}
+fi
+status_check $?
+
+print_head "Delete Old Content"
+rm -rf /app/* &>>${log_file}
+status_check $?
+
+print_head "Downloading App Content"
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log_file}
+status_check $?
+cd /app
+
+print_head "Extracting App Content"
+unzip /tmp/user.zip &>>${log_file}
+status_check $?
+
+print_head "Installing NodeJS Dependencies"
+npm install &>>${log_file}
+status_check $?
+
+print_head "Copy SystemD Service File"
+cp ${code_dir}/configs/user.service /etc/systemd/system/user.service &>>${log_file}
+status_check $?
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+status_check $?
+
+print_head "Enable User Service "
+systemctl enable user &>>${log_file}
+status_check $?
+
+print_head "Start User Service"
+systemctl restart user &>>${log_file}
+status_check $?
+
+print_head "Copy MongoDB Repo File"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+status_check $?
+
+print_head "Install Mongo Client"
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+
+print_head "Load Schema"
+mongo --host mongodb.devopsb71.online </app/schema/user.js &>>${log_file}
+status_check $?

1fcbebac0da2080e7524e9c843a0655de4adb989
Raghu K <raghuk.vit@gmail.com>
2023-02-20 07:46:15 +0530

    Update roboshop-shell/

--- frontend.sh
+++ frontend.sh
@@ -2,25 +2,32 @@ source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
+echo $?
 
 print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+echo $?
 
 print_head "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+echo $?
 
 print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+echo $?
 
 print_head "Copying Nginx Config for RoboShop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+echo $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
+echo $?
 
 print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
+echo $?
 
 
 ## If any command is errored or failed, we need to stop the script

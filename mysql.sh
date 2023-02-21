9f6a4a9da56a5d76e66339f56dc5f6c6da930036
Raghu K <raghuk.vit@gmail.com>
2023-02-21 08:02:38 +0530

    Update roboshop-shell/

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@ source common.sh
 
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then
-  echo "Missing MySQL Root Password argument"
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
   exit 1
 fi
 

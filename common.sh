26b549b690262dbb25850bc147a8d4068ccf8aa3
Raghu K <raghuk.vit@gmail.com>
2023-02-21 08:28:53 +0530

    Update roboshop-shell/

--- common.sh
+++ common.sh
@@ -16,6 +16,24 @@ status_check() {
   fi
 }
 
+systemd_setup() {
+  print_head "Copy SystemD Service File"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "Reload SystemD"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+
+  print_head "Enable ${component} Service "
+  systemctl enable ${component} &>>${log_file}
+  status_check $?
+
+  print_head "Start ${component} Service"
+  systemctl restart ${component} &>>${log_file}
+  status_check $?
+}
+
 schema_setup() {
   if [ "${schema_type}" == "mongo" ]; then
     print_head "Copy MongoDB Repo File"
@@ -29,18 +47,18 @@ schema_setup() {
     print_head "Load Schema"
     mongo --host mongodb.devopsb71.online </app/schema/${component}.js &>>${log_file}
     status_check $?
+  elif [ "${schema_type}" == "mysql" ]; then
+    print_head "Install MySQL Client"
+    yum install mysql -y
+    status_check $?
+
+    print_head "Load Schema"
+    mysql -h mysql.devopsb71.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+    status_check $?
   fi
 }
 
-nodejs() {
-  print_head "Configure NodeJS Repo"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-  status_check $?
-
-  print_head "Install NodeJS"
-  yum install nodejs -y &>>${log_file}
-  status_check $?
-
+app_prereq_setup() {
   print_head "Create Roboshop User"
   id roboshop  &>>${log_file}
   if [ $? -ne 0 ]; then
@@ -66,28 +84,47 @@ nodejs() {
   print_head "Extracting App Content"
   unzip /tmp/${component}.zip &>>${log_file}
   status_check $?
+}
+
+nodejs() {
+  print_head "Configure NodeJS Repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+
+  print_head "Install NodeJS"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+
+  app_prereq_setup
 
   print_head "Installing NodeJS Dependencies"
   npm install &>>${log_file}
   status_check $?
 
-  print_head "Copy SystemD Service File"
-  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
-  status_check $?
-
-  print_head "Reload SystemD"
-  systemctl daemon-reload &>>${log_file}
-  status_check $?
-
-  print_head "Enable ${component} Service "
-  systemctl enable ${component} &>>${log_file}
-  status_check $?
-
-  print_head "Start ${component} Service"
-  systemctl restart ${component} &>>${log_file}
-  status_check $?
-
   schema_setup
 
+  systemd_setup
+
 }
+
+java() {
+
+  print_head "Install Maven"
+  yum install maven -y &>>${log_file}
+  status_check $?
+
+  app_prereq_setup
+
+  print_head "Download Dependencies & Package"
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+  status_check $?
+
+  # Schema Setup Function
+  schema_setup
+
+  # SystemD Function
+  systemd_setup
+}
+
 
